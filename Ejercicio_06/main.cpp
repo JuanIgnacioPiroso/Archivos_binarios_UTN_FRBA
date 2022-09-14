a5b5291820851ebc7ad173d65e94b2db80c6d53e
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-09-14 20:34:02 -0300

    ejercicio_06 terminado

--- Ejercicio_06/main.cpp
+++ Ejercicio_06/main.cpp
@@ -2,9 +2,48 @@
 #include <stdio.h>
 using namespace std;
 
-
+struct Alumno
+{
+    char nombre[30];
+    char apellido[30];
+    int legajo;
+    int dni;
+    int fechaNacimiento;
+};
 
 int main()
 {
+    FILE *arch = fopen("alumnos.dat", "wb");
+    Alumno alumn;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Ingrese nombre: ";
+        cin.getline(alumn.nombre, 30);
+        cout << "Ingrese apellido: ";
+        cin.getline(alumn.apellido, 30);
+        cout << "Ingrese legajo: ";
+        cin >> alumn.legajo;
+        cout << "Ingrese dni: ";
+        cin >> alumn.dni;
+        cout << "Ingrese fecha de nacimiento: ";
+        cin >> alumn.fechaNacimiento;
+        cin.ignore();
+        fwrite(&alumn, sizeof(Alumno), 1, arch);
+    }
+
+    
+    fclose(arch);
+
+    arch = fopen("alumnos.dat", "rb");
+
+    while (fread(&alumn, sizeof(Alumno), 1, arch))
+    {
+        cout << alumn.nombre << endl;
+        cout << alumn.apellido << endl;
+        cout << alumn.legajo << endl;
+        cout << alumn.dni << endl;
+        cout << alumn.fechaNacimiento << endl;
+        cout << "-----------------" << endl;
+    }
 
 }
